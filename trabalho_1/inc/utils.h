72d9270b3245ec62e6de07613d69177739606ae0
Diego França <diegobmf@gmail.com>
2021-03-19 20:40:49 -0300

    Adicionando um arquivo para hospedar funções utilitárias

--- trabalho_1/inc/utils.h
+++ trabalho_1/inc/utils.h
@@ -1,6 +1,10 @@
 #ifndef UTILS_H_
 #define UTILS_H_
 
+#define TEMPO_50MS 50000000L
+#define TEMPO_500US 5L
+
 void espera_nsecs(long nsecs);
+int checa_temp_valida(float temperatura);
 
 #endif
--- trabalho_1/src/utils.c
+++ trabalho_1/src/utils.c
@@ -7,3 +7,16 @@ void espera_nsecs(long nsecs)
     tempo_espera.tv_nsec = nsecs;
     clock_nanosleep(CLOCK_MONOTONIC, 0, &tempo_espera, NULL);
 }
+
+int checa_temp_valida(float temperatura)
+{
+    if(temperatura < 20.0 || temperatura > 100.0)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+
+}
